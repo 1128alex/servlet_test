99df18b627a2b30987cfdf8073cbe447443b794e
dlwldnjs <1128alex@gmail.com>
2022-12-07 23:35:28 +0900

    초기설정

---
+++ src/main/java/com/test/lesson01/TestClass.java
@@ -0,0 +1,20 @@
+package com.test.lesson01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestClass extends HttpServlet {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	}
+}
